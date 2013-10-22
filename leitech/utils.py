c8ab6ec68689a3a6960333f584824fd87addb772
Luan fonseca de farias <luanfonceca@gmail.com>
2013-10-22 15:21:30 -0300

    Corrigindo o erro: ImproperlyConfigured: AUTH_USER_MODEL.

--- leitech/utils.py
+++ leitech/utils.py
@@ -7,22 +7,11 @@ Criado por Luan Fonseca em 17/06/2013.
 """
 
 from django.utils import timezone
-from django.http import HttpResponse
 from django.core.mail import send_mail
 from django import forms
-from simplejson import dumps as json_dumps
-
 
 def now():
     return timezone.now()
-
-
-def write(obj, **kw):
-    return json_dumps(obj, encoding='utf-8', **kw)
-
-
-def JsonHttpResponse(data, **kw):
-	return HttpResponse(write(data, **kw))
 
 
 class HistoryModelForm(forms.ModelForm):
