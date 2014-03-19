4e3ab22139af2f9f62127fd32513ac586dda2dbf
Luan fonseca de farias <luanfonceca@gmail.com>
2014-03-19 02:29:00 -0300

    Tentando corrigir o Procfile. v4

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -w 3 --pythonpath leitech leitech.wsgi
--- leitech/settings.py
+++ leitech/settings.py
@@ -66,7 +66,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -79,6 +79,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
